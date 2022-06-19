c9d7222a2e8524786dac77d34008fffe807d98a0
laziz746 <38212585+laziz746@users.noreply.github.com>
2022-06-19 14:30:48 +0300

    HW7

---
+++ src/HomeWork07/Cat.java
@@ -0,0 +1,40 @@
+package HomeWork07;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Cat {
+
+    boolean satiety;
+    private String name;
+
+    public Cat(String name) {
+
+        this.name = name;
+        satiety = false;
+    }
+
+    public boolean isSatiety() {
+        return satiety;
+    }
+
+    public void isSatietyCats() {
+
+        if (isSatiety()) {
+            System.out.println("Котик " + name + " сыт ");
+        } else {
+            System.out.println("Котик " + name + " голоден");
+        }
+    }
+
+    public void eat(Plate plate) {
+        int catEatFoodCount = ThreadLocalRandom.current().nextInt(4) + 3;
+        plate.decreaseFood(catEatFoodCount);
+        if (plate.isTrue()) {
+            satiety = true;
+//            System.out.println("Котик " + name + " съел " + catEatFoodCount + "корма");
+        } else {
+//            System.out.println("Котику " + name + " не хватило " + "корма");
+        }
+
+    }
+}
