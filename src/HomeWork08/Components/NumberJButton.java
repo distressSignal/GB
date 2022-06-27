3c7608411092d4d25d60cb53ba2cddc4ff6e3acc
laziz746 <38212585+laziz746@users.noreply.github.com>
2022-06-27 21:29:32 +0300

    HW8

---
+++ src/HomeWork08/Components/NumberJButton.java
@@ -0,0 +1,12 @@
+package HomeWork08.Components;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class NumberJButton extends JButton {
+    public NumberJButton(String text) {
+        super(text);
+        setFont(new Font("TimesRoman", Font.PLAIN, 25));
+        setBackground(new Color(154, 195, 231));
+    }
+}
---
+++ src/HomeWork08/Main.java
@@ -0,0 +1,7 @@
+package HomeWork08;
+
+public class Main {
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+        new HomeWork08.ApplicationForm("Calculator v1.0 beta");
+    }
+}
